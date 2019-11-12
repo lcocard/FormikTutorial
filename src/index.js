09491e648c72e8d6498d78c2efcb6cb95ffc92c6
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-11-12 03:20:04 -0500

    updated 3 className form4

--- src/index.js
+++ src/index.js
@@ -13,75 +13,75 @@ const DiagnosisForm4 = () => {
         <div className="card-image">
           <img src="https://lorempixel.com/100/190/nature/6"  alt="Diagnosis 1"/>
         </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <p  class="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p  className="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
           </div>
-          <div class="card-action">
+          <div className="card-action">
             <a href="https://github.com/Garciat427/Med-2.0/projects/1">Med2.0 Project</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="col s12 m7">
-      <h2 class="header">Diagnosis 2</h2>
-      <div class="card horizontal">
-        <div class="card-image">
+    <div className="col s12 m7">
+      <h2 cclassName="header">Diagnosis 2</h2>
+      <div className="card horizontal">
+        <div className="card-image">
           <img src="https://lorempixel.com/100/190/nature/6"  alt="Diagnosis 2"/>
         </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <p  class="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p  className="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
           </div>
-          <div class="card-action">
+          <div className="card-action">
             <a href="https://github.com/Garciat427/Med-2.0/projects/1">Med2.0 Project</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="col s12 m7">
-      <h2 class="header">Diagnosis 3</h2>
-      <div class="card horizontal">
-        <div class="card-image">
+    <div className="col s12 m7">
+      <h2 className="header">Diagnosis 3</h2>
+      <div className="card horizontal">
+        <div className="card-image">
           <img src="https://lorempixel.com/100/190/nature/6"  alt="Diagnosis 3"/>
         </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <p  class="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p  className="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
           </div>
-          <div class="card-action">
+          <div className="card-action">
             <a href="https://github.com/Garciat427/Med-2.0/projects/1">Med2.0 Project</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="col s12 m7">
-      <h2 class="header">Diagnosis 4</h2>
-      <div class="card horizontal">
-        <div class="card-image">
+    <div className="col s12 m7">
+      <h2 className="header">Diagnosis 4</h2>
+      <div className="card horizontal">
+        <div className="card-image">
           <img src="https://lorempixel.com/100/190/nature/6"  alt="Diagnosis 4"/>
         </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <p  class="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p  className="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
           </div>
-          <div class="card-action">
+          <div className="card-action">
             <a href="https://github.com/Garciat427/Med-2.0/projects/1">Med2.0 Project</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="col s12 m7">
-      <h2 class="header">Diagnosis 5</h2>
-      <div class="card horizontal">
-        <div class="card-image">
+    <div className="col s12 m7">
+      <h2 className="header">Diagnosis 5</h2>
+      <div className="card horizontal">
+        <div className="card-image">
           <img src="https://lorempixel.com/100/190/nature/6"  alt="Diagnosis 5"/>
         </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <p  class="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p  className="grey-text text-darken-2 flow-text">I am a very simple card. I am good at containing small bits of information.</p>
           </div>
-          <div class="card-action">
+          <div className="card-action">
             <a href="https://github.com/Garciat427/Med-2.0/projects/1">Med2.0 Project</a>
           </div>
         </div>
